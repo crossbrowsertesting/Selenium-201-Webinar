b482c2d2974531830e5c510e6184a95a5e4b0b96
Chase Cook <chase@crossbrowsertesting.com>
2020-01-28 08:10:57 -0600

    put creds elsewhere

---
+++ creds.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class creds(Enum):
+    #def __init__():
+ 
+    email_address = "meh@meh.io"
+    password = ""
+
+    # def email_address(self):
+    #     return self.email_address
+
+    # def password(self):
+    #     return self.password
